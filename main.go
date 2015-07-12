0ce9c5891817167d40598edf4e0e2bc0c2efefb8
Mihir Singh <git.service@mihirsingh.com>
2015-07-12 12:52:14 -0700

    Formatted main.go

--- main.go
+++ main.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-  "os"
-  "strings"
-  "github.com/codegangsta/cli"
+	"github.com/codegangsta/cli"
+	"os"
+	"strings"
 )
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "dockerfileview"
-  app.Version = "0.1.0"
-  app.HideHelp = true
-  app.Flags = []cli.Flag{
-      cli.HelpFlag,
-      cli.BoolFlag{
-        Name: "text, t",
-        Usage: "text output without color and syntax highlight",
-      },
-  }
-  app.Usage = "a public Dockerfile viewer"
-  app.Action = func(c *cli.Context) {
-    if len(os.Args)>1 {
-      colorFlag := true
-      if c.Bool("text") {
-        colorFlag = false
-      }
-      if strings.Index(os.Args[len(os.Args)-1], "Dockerfile")>=0 {
-        inFile, _ := os.Open(os.Args[len(os.Args)-1])
-        defer inFile.Close()
-        parseDockerfile(os.Args[len(os.Args)-1], "", inFile, colorFlag)
-      } else {
-        parseDockerfile("(Direct Input from CLI)", "", strings.NewReader("FROM " +os.Args[len(os.Args)-1]), colorFlag)
-      }
-    }
-  }
-  app.Run(os.Args)
+	app := cli.NewApp()
+	app.Name = "dockerfileview"
+	app.Version = "0.1.0"
+	app.HideHelp = true
+	app.Flags = []cli.Flag{
+		cli.HelpFlag,
+		cli.BoolFlag{
+			Name:  "text, t",
+			Usage: "text output without color and syntax highlight",
+		},
+	}
+	app.Usage = "a public Dockerfile viewer"
+	app.Action = func(c *cli.Context) {
+		if len(os.Args) > 1 {
+			colorFlag := true
+			if c.Bool("text") {
+				colorFlag = false
+			}
+			if strings.Index(os.Args[len(os.Args)-1], "Dockerfile") >= 0 {
+				inFile, _ := os.Open(os.Args[len(os.Args)-1])
+				defer inFile.Close()
+				parseDockerfile(os.Args[len(os.Args)-1], "", inFile, colorFlag)
+			} else {
+				parseDockerfile("(Direct Input from CLI)", "", strings.NewReader("FROM "+os.Args[len(os.Args)-1]), colorFlag)
+			}
+		}
+	}
+	app.Run(os.Args)
 }
