a744d0ced7b7069a8ff731376d4c9cff721ccf77
Kei Sawada <k@swd.cc>
2015-07-27 23:22:03 +0900

    increment a minor version from 0.1.0 to 0.1.1

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "dockerfileview"
-	app.Version = "0.1.0"
+	app.Version = "0.1.1"
 	app.HideHelp = true
 	app.Flags = []cli.Flag{
 		cli.HelpFlag,
